c476d0e62868a230aede80c81b36a3bce58d78ea
Evgeniy Golovin <edgolovin@yandex.ru>
2021-03-11 22:48:28 +0700

    refactor: make some fixes in curl.sh script

--- scripts/curl.sh
+++ scripts/curl.sh
@@ -1,4 +1,5 @@
 while read line; do
 	filename=$( echo ${line:14} | tr / _);
-	curl --url $line  --output $filename ;
-done < q2021-01.txt
+	echo $filename;
+	curl -s --url $line  --output $filename ;
+done < all_urls_before_2019.txt
